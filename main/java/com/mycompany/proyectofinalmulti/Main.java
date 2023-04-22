4cb88892b84dd446bbfc79c38e6249e50b5b2439
JhonmaSG <jmserranog@udistrital.edu.co>
2023-04-22 08:33:21 -0500

    Inicializamos el repositorio

---
+++ main/java/com/mycompany/proyectofinalmulti/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinalmulti;
+
+/**
+ *
+ * @author Jhon
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hola :D");
+        System.out.println("Hello World!!!!");
+        System.out.println("Inicializando proeycto de multinivel!!!!");
+    }
+}
